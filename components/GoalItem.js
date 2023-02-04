a5c9936265f30637a670f63d08fa7afe8174043e
riyoneri <lionkaneza@gmail.com>
2023-02-04 21:23:48 +0200

    Android ripple & ios alternative

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,22 +2,29 @@ import { StyleSheet, View, Text, Pressable } from "react-native"
 
 const GoalItem = props => {
 
-    return <Pressable onPress={props.onDeleteGoal.bind(this, props.goalData._id)}>
-        <View style={styles.goalItem}>
+    return <View style={styles.goalItem}>
+        <Pressable
+            android_ripple={{ color: "#210644" }}
+            onPress={props.onDeleteGoal.bind(this, props.goalData._id)}
+            style={({ pressed }) => pressed && styles.pressedItem}
+        >
             <Text style={styles.goalText}>{props.goalData.text}</Text>
-        </View>
-    </Pressable>
+        </Pressable>
+    </View>
 }
 
 const styles = StyleSheet.create({
     goalItem: {
         margin: 8,
-        padding: 8,
         borderRadius: 6,
         backgroundColor: "#5e0acc"
     },
-    goalText: {
+    pressedItem: {
         color: "white"
+    },
+    goalText: {
+        color: "white",
+        padding: 8,
     }
 })
 
